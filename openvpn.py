7cfb59ad6328240abad0bc6f5f48ed16bae54487
jhoexii <70842395+jhoexii@users.noreply.github.com>
2020-11-10 12:03:04 +0800

    Update openvpn.py

--- openvpn.py
+++ openvpn.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-# GTM NO load By: Ykcir Ogotip Caayon
+# python proxy by: jhoexii
 import socket, threading, thread, select, signal, sys, time
 from os import system
 system("clear")
